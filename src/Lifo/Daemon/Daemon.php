ca41fea33745e6389c7b3bf7a4ef0bd837492aff
John Hitchins <john.hitchins@goodmorningbob.net>
2017-10-26 10:03:32 +0100

    Call pcntl_signal_dispatch in hte daemon loop

--- src/Lifo/Daemon/Daemon.php
+++ src/Lifo/Daemon/Daemon.php
@@ -1733,6 +1733,8 @@ abstract class Daemon
         $this->loopStart = microtime(true);
         $this->loopIterations = 0;
         while ($this->parent && !$this->shutdown) {
+            pcntl_signal_dispatch();
+
             $this->loopIterations++;
             $start = microtime(true);
 
